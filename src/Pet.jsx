34b4f722458d35a16896883f10f83816ab9363a9
Anany Agrawal <ananyagrawal698@gmail.com>
2023-02-12 21:41:58 +0530

    Update style

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 
 // const Pet = (petob) => {
@@ -8,14 +9,33 @@ import React from "react";
 //     ]);
 //   };
 
-const Pet = (petob) => {
-    return(
-        <div>
-            <h1>{petob.name}</h1>
-            <h2>{petob.animal}</h2>
-            <h2>{petob.breed}</h2>
-        </div>
-    )
-}
+// const Pet = (petob) => {
+//     return(
+//         <div>
+//             <h1>{petob.name}</h1>
+//             <h2>{petob.animal}</h2>
+//             <h2>{petob.breed}</h2>
+//         </div>
+//     )
+// }
+
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
+
+  return (
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+      </div>
+    </a>
+  );
+};
 
 export default Pet;
