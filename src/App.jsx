0f260c785cc21f90972d25f1ce6aeae600102c3d
Anany Agrawal <ananyagrawal698@gmail.com>
2023-02-15 22:44:19 +0530

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Details from "./Details";
-import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 
 // const App = () => {
